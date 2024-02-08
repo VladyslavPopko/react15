1af30f9904ba7912a351282bffcaf342decebf71
VladPopko <vladpopko@gmail.com>
2024-02-08 11:51:36 +0200

    hw15

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,31 @@ import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router";
 import "./style/reset.css";
 import "./style/main.css";
-import Home from "./pages/Home";
 import PageNotFound from "./pages/PageNotFound";
-import Menu from "./pages/Menu";
-import Cart from "./pages/Cart";
-import NewOrder from "./pages/NewOrder";
-import Order from "./pages/Order";
+import { Suspense, lazy } from "react";
+
+const HomeLazy = lazy(() => import("./pages/Home"));
+const MenuLazy = lazy(() => import("./pages/Menu"));
+const CartLazy = lazy(() => import("./pages/Cart"));
+const NewOrderLazy = lazy(() => import("./pages/NewOrder"));
+const OrderLazy = lazy(() => import("./pages/Order"));
 
 function App() {
   return (
     <>
       <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/menu" element={<Menu />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="/order/new" element={<NewOrder />}></Route>
-        <Route path="/order/:id" element={<Order />}></Route>
+      <Suspense fallback={<h1>Loading ...</h1>}>
+        <Routes>
+          <Route path="/" element={<HomeLazy />}></Route>
+          <Route path="/menu" element={<MenuLazy />}></Route>
+          <Route path="/cart" element={<CartLazy />}></Route>
+          <Route path="/order/new" element={<NewOrderLazy />}></Route>
+          <Route path="/order/:id" element={<OrderLazy />}></Route>
 
-        <Route path="*" element={<PageNotFound />}></Route>
-      </Routes>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </Suspense>
     </>
   );
 }
